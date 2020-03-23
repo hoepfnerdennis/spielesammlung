57c9c071c94c95344e57d46b0f4c049582507785
Dennis Höpfner <dennis.hoepfner@dm.de>
2020-03-23 13:57:34 +0100

    fade in animation

--- src/Entry/index.tsx
+++ src/Entry/index.tsx
@@ -1,4 +1,4 @@
-import React, { memo, createRef } from 'react';
+import React, { memo, createRef, useEffect } from 'react';
 import styles from './styles.module.css';
 import { IGame } from '../Store/types';
 import useIntersection from './intersection';
@@ -16,25 +16,36 @@ const Entry: React.SFC<IGame> = ({
   image,
 }): JSX.Element => {
   const entryRef = createRef<HTMLDivElement>();
-  const [intersecting] = useIntersection(entryRef, true, { rootMargin: '100px' });
+  const [intersecting] = useIntersection(entryRef, true);
+  const [intersectingWithMargin] = useIntersection(entryRef, true, { rootMargin: '200px' });
+
+  useEffect(() => {
+    if (intersectingWithMargin) {
+      const loadImage = new Image();
+      loadImage.src = image;
+    }
+  }, [intersectingWithMargin, image]);
+
   return (
     <div className={styles.border} ref={entryRef}>
-      <div className={`${styles.container} ${favorite ? styles.fav : ''}`}>
-        <div className={styles.imageContainer}>
-          {intersecting && <img src={image} alt={`Bild von ${name}`} className={styles.image} />}
+      {intersecting && (
+        <div className={`${styles.container} ${favorite ? styles.fav : ''}`}>
+          <div className={styles.imageContainer}>
+            <img src={image} alt={`Bild von ${name}`} className={styles.image} />
+          </div>
+          <div className={styles.info}>
+            <h2 className={styles.name}>{name}</h2>
+            <p className={styles.description}>{description}</p>
+            <Features
+              playersFrom={playersFrom}
+              playersTo={playersTo}
+              age={age}
+              duration={duration}
+              simpleRules={simpleRules}
+            />
+          </div>
         </div>
-        <div className={styles.info}>
-          <h2 className={styles.name}>{name}</h2>
-          <p className={styles.description}>{description}</p>
-          <Features
-            playersFrom={playersFrom}
-            playersTo={playersTo}
-            age={age}
-            duration={duration}
-            simpleRules={simpleRules}
-          />
-        </div>
-      </div>
+      )}
     </div>
   );
 };
